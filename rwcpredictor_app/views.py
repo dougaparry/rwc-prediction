7935854466da9d111047d7afc506cc092050379e
Doug Parry <dougparry2@gmail.com>
2015-08-21 22:05:56 +0200

    first link with domino

--- rwcpredictor_app/views.py
+++ rwcpredictor_app/views.py
@@ -4,7 +4,38 @@ from django.shortcuts import RequestContext
 from django.http import HttpResponseRedirect
 #from models import MODELNAME
 
+#necessary for requesting & handling data from Domino API
+import unirest
+import json
+import yaml
+
 #Displayling the home page:
 
+def recieveProbs(team1, team2):
+    teamA = team1
+    teamB = team2
+
+    response = unirest.post("https://app.dominodatalab.com/v1/Arnu/rwcPrediction/endpoint",
+        headers={
+            "X-Domino-Api-Key": "iuGyjiXexOrCzjFPsNs3mkBRO2ztIvxhHBEYmtfVmiUjbaUbb4HeS5E0x8Uk3WhP",
+            "Content-Type": "application/json" },
+        params=json.dumps({
+            "parameters": [teamA, teamB]})
+            )
+
+    #extract information from response:
+    response_data = yaml.load(response.raw_body)
+    probability = response_data['result']
+    return probability
+
 def homepage(request):
-    return render(request, "base.html")
+    homeTeam = 'South Africa'
+    awayTeam = 'New Zealand'
+
+    probability = recieveProbs(homeTeam, awayTeam)
+    if probability[0] < probability[1]:
+        winner = homeTeam
+    else:
+        winner = awayTeam
+
+    return render(request, "home.html",{'winner': winner,'teamA': homeTeam, 'teamB':awayTeam})
