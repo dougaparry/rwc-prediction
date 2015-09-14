bc9cc052079d09789077ae4f3d1e74740c1b8d0e
Doug Parry <dougparry2@gmail.com>
2015-09-14 21:22:16 +0200

    added link to amazon s3

--- rwcpredictor_app/views.py
+++ rwcpredictor_app/views.py
@@ -2,19 +2,20 @@
 from django.shortcuts import render, render_to_response
 from django.shortcuts import RequestContext
 from django.http import HttpResponseRedirect
-#from models import MODELNAME
-
 from django.conf import settings
 import os
 import csv
+import urllib2
 
 def homepage(request):
+    url = 'https://s3.eu-central-1.amazonaws.com/rwcpredictors3bucket/predictions.csv'
     data = list()
 
-    with open('predictions.csv') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            data.append(row)
+    response = urllib2.urlopen(url)
+    cr = csv.reader(response)
+
+    for row in cr:
+        data.append(row)
 
     return render(request,"home.html", {"data": data})
 
